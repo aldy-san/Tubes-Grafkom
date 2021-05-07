9b0dbb5872e8c9e401dcbe9abad7891dd8a8bcb7
aldy-san <nsmaulidi12@gmail.com>
2021-05-07 08:15:49 +0700

    Buat Latar

--- main.cpp
+++ main.cpp
@@ -18,15 +18,21 @@ struct Pallette {
     void hijau() {
         rgb(39, 174, 96);
     }
+    void hijau2(){
+    	rgb(122,214,112);
+	}
+	void hijau3(){
+		rgb(46, 204, 113);
+	}
     void hijauTua() {
         rgb(0, 150, 0);
+    }
+    void hijauTua2() {
+        rgb(16, 172, 132);
     }
     void hijauMuda() {
         rgb(110,200,150);
     }
-    void hijau2(){
-    	rgb(122,214,112);
-	}
     void kuning(){
     	rgb(241, 196, 15);
 	}
@@ -167,7 +173,21 @@ void gunung(float scale, float posX, float posY){
 	glEnd();
 	glPopMatrix();
 }
-
+void gunung2(float scale, float posX, float posY){
+	glPushMatrix();
+	glScalef(scale, scale, 1.0);
+	glTranslatef(posX, posY, 0.0);
+	painter.abuabuTua();
+    segitiga(0, 450, 300, 0.0, 0.0);
+    painter.hampirPutih();
+    segitiga(0, 150, 100, 0.0, 200.0);
+    painter.hampirPutih();
+    segitiga(180, 20, 40, 40.0, 200.0);
+    painter.abuabuTua();
+    segitiga(0, 15, 20, -40.0, 200.0);
+	glEnd();
+	glPopMatrix();
+}
 void pohon(float scale, float posX, float posY){
 	glPushMatrix();
 	glScalef(scale, scale, 1.0);
@@ -196,20 +216,20 @@ void kincirAngin(float scale, float posX, float posY){
 	glPopMatrix();
 }
 
-void pohon2(float posX, float posY, float scale){
+void pohon2(float scale, float posX, float posY){
     glPushMatrix();
     glTranslatef(posX, posY, 0.0);
     glScalef(scale, scale, 0.0);
 
     //batang
     painter.cokelat2();
-    persegi(0, 0.5, 1.25, 0, 0);
+    persegi(0, 0.6, 1.55, 0, 0);
 
     //daun
-    painter.hijau();
-    segitiga(0, 5, 3, 0, 0);
-    segitiga(0, 5, 3, 0, 1);
-    segitiga(0, 5, 3, 0, 2);
+    painter.hijauTua();
+    segitiga(0, 4, 3, 0, 0);
+    segitiga(0, 4, 3, 0, 1);
+    segitiga(0, 4, 3, 0, 2);
 
     glPopMatrix();
 }
@@ -270,90 +290,56 @@ void kapal(float scale, float posX, float posY){
 	glPopMatrix();
 }
 
-void gradasiAir(){
-	glPushMatrix();
-	glTranslatef(0, -240, 0.0f);
-	glRotatef(0, 0.0f, 0.0f, 1.0f);
-	glBegin(GL_QUADS);
-    painter.biruLautTua();
-    glVertex2f(900, 110);
-    glVertex2f(-900, 110);
-	painter.biruLaut();
-    glVertex2f(-900, -110);
-    glVertex2f(900,-110);
-    glEnd();
-    glPopMatrix();
-}
-
 void initGL(){
-	glClearColor(255/255., 234/255., 210/255., 1.0);
+//	rgb(145,216,232);
+	glClearColor(145/255., 216/255., 232/255., 1.0);
 }
 
 void render(void) {
     glClear(GL_COLOR_BUFFER_BIT);
     //MATAHARI
     painter.kuningCerah();
-    lingkaran(55, -275, 200);
+    lingkaran(55, -275, 300);
     painter.kuning();
-    lingkaran(40, -275, 200);
-    //GUNUNG
-    gunung(0.9, -320., -130.);
+    lingkaran(40, -275, 300);
+	//GUNUNG
+	gunung2(1.5, 0., -50.);
+	gunung(0.9, -320., -130.);
     gunung(1.3, -150., -100.);
     gunung(1., -50., -100.);
     gunung(0.8, 50., -130.);
     gunung(0.9, 130., -70.);
     gunung(1.5, 160., -70.);
-
     //BUKIT
-	painter.hijauMuda();
-    lingkaran(300, 225, -200);
+    painter.hijauTua2();
+    persegi(0, 700, 100, 0, -75);
+    painter.hijau3();
+    lingkaran(275, -100, -270);
+	painter.hijau2();
+    lingkaran(300, 150, -400);
     painter.hijau();
-    lingkaran(250, -200, -200);
-    painter.hijau2();
-    lingkaran(130, 90, -170);
-
-	    //POHON
-	    pohon(0.4, 200.0, -300.0);
-	    pohon(0.6, 200.0, -190.0);
-	    pohon(0.4, 600.0, -300.0);
-	    pohon(0.4, -950.0, -300.0);
-	    pohon(0.6, -580.0, -190.0);
-	    pohon(0.4, -700.0, -300.0);
-	    pohon(0.4, -450.0, -300.0);
-	    pohon(0.4, -300.0, -300.0);
-	    pohon(0.6, 370, -190.0);
-	    pohon(0.5, 850, -240.0);
-
-	    //KINCIR ANGIN
-	    kincirAngin(0.85, 350.0, -80.0);
-	    kincirAngin(0.50, 720.0, -200.0);
-	    kincirAngin(0.50, 720.0, -200.0);
-
-    //LAUT
-    gradasiAir();
-    	//KAPAL
-    	kapal(0.5, 280.0, -500.0);
-
-    //Tembok
-    rumah(20, 20, 2);
-    pohon2(100, 150, 20);
+    lingkaran(500, -180, -600);
+    
+//    lingkaran(130, 90, -170);
+//
+//    //Tembok
+//    rumah(20, 20, 2);
+    pohon2(60, 350, -300);
     //AWAN dari kiri->kanan
     awan(1.25, -220.0, 120.0);
     awan2(1., -100.0, 285.0);
     awan(0.8, 90.0, 225.0);
     awan2(1.5, 180.0, 160.0);
     glFlush();
-
-
 }
 
 int main(int argc, char* argv[]) {
     glutInit(&argc, argv);
-    glutInitWindowSize(900, 700);
+    glutInitWindowSize(700, 700);
     glutInitWindowPosition(10, 10);
     glutInitDisplayMode(GLUT_DEPTH | GLUT_SINGLE | GLUT_RGBA);
     glutCreateWindow("Pemandangan");
-    gluOrtho2D(-450., 450., -350., 350.);
+    gluOrtho2D(-350., 350., -350., 350.);
     glutDisplayFunc(render);
     initGL();
     glutMainLoop();
